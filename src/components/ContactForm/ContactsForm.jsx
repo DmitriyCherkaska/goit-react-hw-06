e7a1b7bb5f5975b041fc09ca97110227134770c5
DmitriyCherkaska <151064884+DmitriyCherkaska@users.noreply.github.com>
2024-06-23 18:54:29 +0300

    adada

--- src/components/ContactForm/ContactsForm.jsx
+++ src/components/ContactForm/ContactsForm.jsx
@@ -1,13 +1,12 @@
 // import style from './ContactForm.module.css';
+import * as Yup from 'yup';
+import MaskedInput from 'react-text-mask';
 import { Field, Form, Formik } from 'formik';
 import { useId } from 'react';
-import * as Yup from 'yup';
 import { ErrorMessage } from 'formik';
-import { nanoid } from 'nanoid';
 import { useDispatch } from 'react-redux';
-import { addContact } from '../../redux/contactsSlice';
-import MaskedInput from 'react-text-mask';
 import { BsBoxArrowUpLeft } from 'react-icons/bs';
+import { addContact } from '../../redux/contactsOps';
 
 const ContactSchema = Yup.object().shape({
   name: Yup.string()
@@ -52,7 +51,10 @@ const ContactForm = ({ setIsFormVisible }) => {
   const dispatch = useDispatch();
 
   const handleSubmit = (values, actions) => {
-    const newContact = { id: nanoid(), ...values };
+    const newContact = {
+      name: values.name,
+      number: values.number,
+    };
     dispatch(addContact(newContact));
     setIsFormVisible(true);
     actions.resetForm();
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,21 +1,18 @@
-// import style from './ContactList.module.css';
+import style from './ContactList.module.css';
 import Contact from '../Contact/Contact';
-import { selectContacts, selectNameFilter } from '../../redux/selectors';
+import { selectFilteredContacts } from '../../redux/selectors';
 import { useSelector } from 'react-redux';
 
 const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectNameFilter);
-  const foundContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase()),
-  );
+  const contacts = useSelector(selectFilteredContacts);
+
   return (
-    <section>
-      <ul>
-        {foundContacts.map(contact => {
+    <section className={style.sectionList}>
+      <ul className={style.list}>
+        {contacts.map(contact => {
           return (
-            <li key={contact.id}>
-              <Contact data={contact} />
+            <li key={contact.id} className={style.item}>
+              <Contact contact={contact} />
             </li>
           );
         })}
