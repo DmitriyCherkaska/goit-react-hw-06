679d9805f9b283c7247ea38007fd0610ee261651
DmitriyCherkaska <151064884+DmitriyCherkaska@users.noreply.github.com>
2024-06-23 19:22:07 +0300

    Revert "asdasdad"
    This reverts commit 0d5a9c847c523d571dabd89ebe85db7a1b446dbd.

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,5 +1,5 @@
 import style from './SearchBox.module.css';
-import ContactForm from '../ContactForm/ContactsForm';
+import ContactForm from '../ContactForm/ContactForm';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeFilter } from '../../redux/filtersSlice';
 import { selectFilter } from '../../redux/selectors';
